91e38ad0f1151c5efb159308562ef1e916038c4e
Ian Edwards <ian.edward@uconn.edu>
2020-01-25 13:47:22 -0500

    Update

---
+++ Module 6 Deliverable_JavaScript/Javascript.js
@@ -0,0 +1,36 @@
+//Grow Element- button1
+document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";;
+
+        });
+
+//Change Element Color (Blue)- button2
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.backgroundColor = "blue";
+
+        });
+
+//Fade Element- button3
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").fadeOut();
+
+        });
+
+//Fade Element- button3
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+//Reset Element- button5
+    document.getElementById("button5").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.backgroundColor = "orange";
+
+});
