181e06d13b278ef89ab0f2c956cea02e1f5d1a49
Ian Edwards <ian.edward@uconn.edu>
2020-01-26 01:43:02 -0500

    update

---
+++ Module 6 Deliverable_JavaScript/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button3").click(function(){
+      $("box").fadeToggle();
+      //$("#div2").fadeToggle("slow");
+      //$("#div3").fadeToggle(3000);
+    });
+  });
